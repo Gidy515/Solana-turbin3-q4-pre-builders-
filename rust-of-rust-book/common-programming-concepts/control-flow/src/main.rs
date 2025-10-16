ed03a1e35b234b46aa727e0ddec5923b55141528
Gideon Okorie <giditech14.com>
2025-10-16 13:20:00 +0100

    ripped loops and other coditionals

--- rust-of-rust-book/common-programming-concepts/control-flow/src/main.rs
+++ rust-of-rust-book/common-programming-concepts/control-flow/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     // if Expressions
     let number = 6;
+
     if number % 4 == 0 {
         println!("number is divisible by 4");
     } else if number % 3 == 0 {
@@ -10,4 +11,65 @@ fn main() {
     } else {
         println!("number is not divisible by 4, 3, or 2");
     }   
+
+    // using if in a let statement
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {}", number);
+
+    // Repetition with Loops (loop, while, for)
+    loop {
+        println!("again!");
+        break;
+    }
+
+    // using loop to return a value
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {result}");
+
+    // Nested loops with labels and breaking to a specific loop
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    // Conditional loops with while
+    let mut number = 3;
+    while number != 0 {
+        println!("{number}");
+        number -= 1;
+    } 
+    println!("LIFTOFF!!!");
+
+    // looping through a collection with for 
+    let a = [10, 20, 30, 40, 50];
+    for element in a {
+        println!("The value of the element in a is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }
