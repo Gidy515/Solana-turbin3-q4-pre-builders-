9d2e042312cabe3b8104163b3d3b0f7cefae92f5
Gideon Okorie <giditech14.com>
2025-10-19 01:59:41 +0100

    more associated functions

--- rust-of-rust-book/structs/methods/src/main.rs
+++ rust-of-rust-book/structs/methods/src/main.rs
@@ -10,6 +10,29 @@ impl Rectangle3 {
     }
 }
 
+// Methods with more parameters
+/*
+ implementing a second method on the Rectangle struct. This time we want an instance of Rectangle to
+take another instance of Rectangle and return true if the second Rectangle can fit completely within self (the first Rectangle);
+otherwise, it should return false. That is, once we’ve defined the can_hold method, we want to be able to write the program shown
+ */
+//#[derive(Debug)]
+struct Rectangle4 {
+    height: u32,
+    width: u32,
+}
+
+impl Rectangle4 {
+    fn area2(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle4) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+//Associated functions
+
 fn main() {
     let rect3 = Rectangle3 {
         height: 50,
@@ -17,4 +40,21 @@ fn main() {
     };
 
     println!("The are of the rectangle is {} and the struct is {:#?}", rect3.area(), rect3);
+
+    // Methods with more parameters (function call)
+    let rect4 = Rectangle4 {
+        width: 30,
+        height: 50,
+    };
+    
+    let rect5 = Rectangle4 {
+        width: 10,
+        height: 40,
+    };
+    let rect6 = Rectangle4 {
+        width: 60,
+        height: 45,
+    };
+    println!("Can rect1 hold rect2? {}", rect4.can_hold(&rect5));
+    println!("Can rect1 hold rect3? {}", rect4.can_hold(&rect6));
 }
