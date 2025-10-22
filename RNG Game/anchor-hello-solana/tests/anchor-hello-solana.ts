7fb9b7c18e654145cf2df4989ff766096fc0a860
Gideon Okorie <giditech14.com>
2025-10-22 02:19:22 +0100

    small change for hello solana

--- RNG Game/anchor-hello-solana/tests/anchor-hello-solana.ts
+++ RNG Game/anchor-hello-solana/tests/anchor-hello-solana.ts
@@ -1,19 +1,3 @@
-/*import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { AnchorHelloSolana } from "../target/types/anchor_hello_solana";
-
-describe("anchor-hello-solana", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
-  const program = anchor.workspace.anchorHelloSolana as Program<AnchorHelloSolana>;
-
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});*/
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorHelloSolana } from "../target/types/anchor_hello_solana";
