ae4e2216a6bbd83cf22153bf1a9a978881aa66a7
Gideon Okorie <giditech14.com>
2025-10-22 11:59:00 +0100

    done initialize instruction

--- RNG Game/anchor-counter/programs/anchor-counter/src/lib.rs
+++ RNG Game/anchor-counter/programs/anchor-counter/src/lib.rs
@@ -6,11 +6,76 @@ declare_id!("CcUMv8RacKYVaXmNyhttz7gUU2wSLMNs4eo2aM3gbRWX");
 pub mod anchor_counter {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<InitializeCounter>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        msg!("Counter initialized with value starting from {counter.count}");
+        //msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn decrement_counter(ctx: Context<Update>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn increment_counter(ctx: Context<Update>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn set_counter(ctx: Context<Update>, value: u8) -> Result<()> {
+        //ctx.accounts.counter.count = value.clone();
+        Ok(())
+    }
+
+    pub fn close_counter(ctx: Context<Close>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeCounter <'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 8, // 8 bytes for discriminator + 8 bytes for u64
+        seeds = [b"counter", user.key().as_ref()],
+        bump,
+    )]
+    pub counter: Account<'info, Counter>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update <'info>{
+    #[account(
+        mut,
+        seeds = [b"counter", user.key().as_ref()],
+        bump,
+    )]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Close <'info>{
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        mut,
+        close = user,
+        seeds = [b"counter", user.key().as_ref()],
+        bump
+    )]
+    pub counter: Account<'info, Counter>
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
+}
